fd200372e18041ffe73d902891a5559f44cd5186
Kirill Seliukov <kselukov@gmail.com>
2026-09-18 15:05:32 +0200

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const map = new Map()
+        for(const num of nums){
+            if(!map.has(num)) {
+                map.set(num, 1)
+                }else{
+                 
+                map.set(num, map.get(num)+1)
+                }
+        }
+        const length = nums.length + 1;
+        const b = Array.from({ length }, () => []);
+        for(const e of map.entries()){
+            b[e[1]].push(e[0])
+        }
+        const result = []
+        let count = k;
+
+         for(let i = b.length - 1; i >= 1; i--){
+            if(b[i].length > 0) {
+                result.push(...b[i])
+                count = count - b[i].length;
+            }
+            if(count === 0) break
+         }
+
+        return result.slice(0,k)
+    }
+}
