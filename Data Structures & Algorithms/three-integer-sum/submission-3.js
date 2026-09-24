bbeb7c5a322d5c5d7c3576fa1d189692a7a929d4
Kirill Seliukov <kselukov@gmail.com>
2026-09-24 14:54:19 +0200

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        nums.sort((a, b) => a - b);
+console.log(nums)
+        const result = []
+
+        for(let i = 0; i < nums.length; i++){
+            if(i > 0 && nums[i] === nums[i-1]) continue
+            const t = nums[i];
+            let l = i+1, r = nums.length-1;
+
+            while(l < r){
+                const s = nums[l] + nums[r] 
+                if(s  === -t ) {
+                    result.push([nums[l], nums[r], nums [i]]);
+                    while(nums[l] === nums[l+1]) {l++;}
+                    while(nums[r] === nums[r-1]) {r--;}
+                    l++;r--;
+                };
+                if(s < -t) {l++; continue;}
+                if(s > -t) {r--; continue;}
+            }
+
+        }
+    console.log(result)
+        return result
+    }
+}
