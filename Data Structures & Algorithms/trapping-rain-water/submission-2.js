7bc322ce7c9b5a2e68452c5b5d15f28741959ba3
Kirill Seliukov <kselukov@gmail.com>
2026-09-25 16:23:58 +0200

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        const leftMax = new Array(height.length)
+        leftMax[0] = height[0]
+        const rightMax = new Array(height.length)
+        rightMax[height.length-1] = height[height.length-1]
+
+        let result = 0
+
+        for (let i = 1; i < height.length; i++){
+            leftMax[i] = Math.max(leftMax[i-1], height[i])
+        }
+      
+        for (let i = height.length-2; i >= 0 ; i--){
+            rightMax[i] = Math.max(rightMax[i+1], height[i])
+        }
+
+        for (let i = 0; i < height.length; i++){
+            result += Math.min(leftMax[i], rightMax[i]) - height[i]
+
+        }
+
+        return result
+    }
+}
